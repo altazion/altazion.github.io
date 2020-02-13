628d4b7644aa1c557798581f9198293ef6ae1dcb
Altazion <michael@carbenay.info>
2020-02-13 20:25:50 +0100

    pas de test en local c'est pénible

--- search.js
+++ search.js
@@ -13,7 +13,7 @@ document.getElementById("searchBox").oninput = function(ev) {
         var tofetch = "https://aidesimplemente.search.windows.net/indexes/aide-altazion/docs?api-version=2019-05-06&&$count=30$filter=type/any(t : t eq 'dev')&query=";
         tofetch += ctn;
         
-        setTimeout(function() {
+        idxSearch = setTimeout(function() {
             fetch(tofetch, { headers: {'Content-Type': 'application/json', 'api-key':'49367045F36D07C494C5589504195957'} }).then(function(response){
                 return response.json();
             }).then(function (data) {
