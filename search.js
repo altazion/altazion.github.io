9e5b9fc936360607ee9e8dfc63c08fd4af01e67e
Altazion <michael@carbenay.info>
2020-02-13 19:53:24 +0100

    petite modif

--- search.js
+++ search.js
@@ -1,9 +1,16 @@
-document.getElementById("SearchBox").onclick = function(ev) {
+document.getElementById("SearchBox").keypress = function(ev) {
+    
     var box = document.getElementById("SearchBox");
     var ctn = box.value;
-    var tofetch = "https://aidesimplemente.search.windows.net/indexes/aide-altazion/docs?api-version=2019-05-06&$filter=type/any(t : t eq 'dev')&query=";
-    tofetch += ctn;
-    fetch({ url : url, headers:[ {'api-key':'49367045F36D07C494C5589504195957'}]).then(function(data){
-        console.log(data);
-    });
+    if(ctn.length==0) {
+
+    }
+    else {
+        var tofetch = "https://aidesimplemente.search.windows.net/indexes/aide-altazion/docs?api-version=2019-05-06&$filter=type/any(t : t eq 'dev')&query=";
+        tofetch += ctn;
+        fetch({ url : url, headers:[ {'api-key':'49367045F36D07C494C5589504195957'}]).then(function(data){
+            console.log(data);
+        });
+    }
+
 }
