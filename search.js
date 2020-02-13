c2a84493c1464d4534d6211ff4d27399a8b3b395
Altazion <michael@carbenay.info>
2020-02-13 20:02:52 +0100

    quelques changements

--- search.js
+++ search.js
@@ -1,14 +1,13 @@
-document.getElementById("SearchBox").keypress = function(ev) {
+document.getElementById("searchBox").oninput = function(ev) {
     
-    var box = document.getElementById("SearchBox");
-    var ctn = box.value;
+    var ctn = ev.target.value;
     if(ctn.length==0) {
 
     }
     else {
         var tofetch = "https://aidesimplemente.search.windows.net/indexes/aide-altazion/docs?api-version=2019-05-06&$filter=type/any(t : t eq 'dev')&query=";
         tofetch += ctn;
-        fetch({ url : url, headers:[ {'api-key':'49367045F36D07C494C5589504195957'}]}).then(function(data){
+        fetch({ url : tofetch, headers:[ {'api-key':'49367045F36D07C494C5589504195957'}]}).then(function(data){
             console.log(data);
         });
     }
