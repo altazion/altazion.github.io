9a13e156c181b4e0bb60cad51efa48c8c0f6cb50
Altazion <michael@carbenay.info>
2020-02-13 20:25:18 +0100

    Ca prend forme petit à petit (le js, pas le look)

--- search.js
+++ search.js
@@ -1,17 +1,25 @@
+var idxSearch = 0;
+
 document.getElementById("searchBox").oninput = function(ev) {
     
+    if(idxSearch>0)
+        clearTimeout(idxSearch);
+
     var ctn = ev.target.value;
     if(ctn.length==0) {
 
     }
     else {
-        var tofetch = "https://aidesimplemente.search.windows.net/indexes/aide-altazion/docs?api-version=2019-05-06&$filter=type/any(t : t eq 'dev')&query=";
+        var tofetch = "https://aidesimplemente.search.windows.net/indexes/aide-altazion/docs?api-version=2019-05-06&&$count=30$filter=type/any(t : t eq 'dev')&query=";
         tofetch += ctn;
-        fetch(tofetch, { mode: 'cors', headers:[ {'Content-Type': 'application/json', 'api-key':'49367045F36D07C494C5589504195957'}]}).then(function(response){
-            return response.json();
-        }).then(function (data) {
-            console.log(data);
-        });
+        
+        setTimeout(function() {
+            fetch(tofetch, { headers: {'Content-Type': 'application/json', 'api-key':'49367045F36D07C494C5589504195957'} }).then(function(response){
+                return response.json();
+            }).then(function (data) {
+                console.log(data.values);
+            });
+        }, 250);
     }
 
 }
