f2cf0a1221313c4e8fdc06b25bc608401241ff6f
Altazion <michael@carbenay.info>
2020-02-13 20:15:04 +0100

    Suite du fetch pour l'aide en ligne

--- search.js
+++ search.js
@@ -7,7 +7,9 @@ document.getElementById("searchBox").oninput = function(ev) {
     else {
         var tofetch = "https://aidesimplemente.search.windows.net/indexes/aide-altazion/docs?api-version=2019-05-06&$filter=type/any(t : t eq 'dev')&query=";
         tofetch += ctn;
-        fetch({ url : tofetch, headers:[ {'api-key':'49367045F36D07C494C5589504195957'}]}).then(function(data){
+        fetch(tofetch, { mode: 'cors', headers:[ {'Content-Type': 'application/json', 'api-key':'49367045F36D07C494C5589504195957'}]}).then(function(response){
+            return response.json();
+        }).then(function (data) {
             console.log(data);
         });
     }
