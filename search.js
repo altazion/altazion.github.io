a5717c5525f64ab9387059fe716afc0125bec8e3
Altazion <teams@altazion.com>
2020-02-13 23:15:20 +0100

    Refactoring header

--- search.js
+++ search.js
@@ -1,25 +1,47 @@
 var idxSearch = 0;
 
-document.getElementById("searchBox").oninput = function(ev) {
-    
-    if(idxSearch>0)
-        clearTimeout(idxSearch);
-
-    var ctn = ev.target.value;
-    if(ctn.length==0) {
-
-    }
-    else {
-        var tofetch = "https://aidesimplemente.search.windows.net/indexes/aide-altazion/docs?api-version=2019-05-06&&$count=30$filter=type/any(t : t eq 'dev')&query=";
-        tofetch += ctn;
+function setupSearch(id, tags) {
+    document.getElementById(id).oninput = function(ev) {
         
-        idxSearch = setTimeout(function() {
-            fetch(tofetch, { headers: {'Content-Type': 'application/json', 'api-key':'49367045F36D07C494C5589504195957'} }).then(function(response){
-                return response.json();
-            }).then(function (data) {
-                console.log(data.values);
-            });
-        }, 250);
-    }
+        if(idxSearch>0)
+            clearTimeout(idxSearch);
 
+        var ctn = ev.target.value;
+        if(ctn.length==0) {
+            hideSearch();
+        }
+        else {
+            var tofetch = "https://aidesimplemente.search.windows.net/indexes/aide-altazion/docs?api-version=2019-05-06&&$count=30$filter=type/any(t :";
+            tofetch += tags;
+            tofetch += ")&query=";
+            tofetch += ctn;
+            
+            idxSearch = setTimeout(function() {
+                fetch(tofetch, { headers: {'Content-Type': 'application/json', 'api-key':'49367045F36D07C494C5589504195957'} }).then(function(response){
+                    return response.json();
+                }).then(function (data) {
+                    showSearch(data);
+                });
+            }, 250);
+        }
+    }
+    var tmp = document.createElement("div");
+    tmp.id = "altazion-search";
+    document.appendChild(tmp);
+
+    tmp = document.createElement("link");
+    tmp.attributes["rel"] = "stylesheet";
+    tmp.attributes["href"] = "https://wxcwxcwx.csqdqs.com/test.css";
+    document.appendChild(tmp);
 }
+
+function showSearch(data) {
+    console.log(data.values);
+    document.getElementById("altazion-search").classList.add("visible");
+}
+
+function hideSearch(){
+    document.getElementById("altazion-search").classList.remove("visible");
+}
+
+setupSearch("searchBox", " t eq 'dev'");
