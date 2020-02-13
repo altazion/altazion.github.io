075f2f81e9bda0fc58a9fd72c4de2484195dfe4b
Altazion <michael@carbenay.info>
2020-02-13 19:53:15 +0100

    création du fichier de search

---
+++ search.js
@@ -0,0 +1,9 @@
+document.getElementById("SearchBox").onclick = function(ev) {
+    var box = document.getElementById("SearchBox");
+    var ctn = box.value;
+    var tofetch = "https://aidesimplemente.search.windows.net/indexes/aide-altazion/docs?api-version=2019-05-06&$filter=type/any(t : t eq 'dev')&query=";
+    tofetch += ctn;
+    fetch({ url : url, headers:[ {'api-key':'49367045F36D07C494C5589504195957'}]).then(function(data){
+        console.log(data);
+    });
+}
